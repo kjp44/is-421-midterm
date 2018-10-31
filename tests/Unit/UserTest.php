f11104205d163ac3a435c145e829076e4d47b9d6
kjp44 <Sabbath51>
2018-10-30 22:05:35 -0400

    Task: Create a unit test to insert a user into the users table.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use Faker\Factory as Faker;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Console\Factories;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUserInsert()
+    {
+        $faker = Faker::create();
+        $user = new User();
+        $user->name = $faker->name;
+        $user->email = $faker->unique()->safeEmail;
+        $user->email_verified_at = now();
+        $user->password = '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm';
+        $user->remember_token = str_random(10);
+        $this->assertTrue(
+            DB::table('users')->insert(
+                [
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'email_verified_at' => $user->email_verified_at,
+                    'password' => $user->password,
+                    'remember_token' => $user->remember_token
+                ]
+            )
+        );
+    }
+}
